e08d8ff64c1263f0650e49a21f6b8108b47ddb76
Ivan Urra <ivan.urra.wd@gmail.com>
2020-12-08 12:02:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route, Link, NavLink, Switch } from 'react-router-dom';
+import LandingPage from './components/LandingPage'
+import './App.css'
 
-function App() {
+export default class App extends Component {
+
+  componentDidMount() {
+    this.setUser()
+  }
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+          <LandingPage/>
+      </div>
+    )
+  }
 }
 
-export default App;
+
---
+++ src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class LandingPage extends Component {
+
+  render() {
+    return (
+      <div className="landing-page" className="landing-page">
+        <h1 className="landing-page-title">MEET & RUN</h1>
+      </div>
+    )
+  }
+
+}
